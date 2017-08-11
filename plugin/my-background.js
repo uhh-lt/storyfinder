6678d4e12f9e83eeb50ea34f410068ed437a5944
Tim Fischer <tim.s.fischer96@googlemail.com>
2017-08-11 17:39:29 +0200

    first commit with some basic features: simple sidebar, simple options / preferences, simple script running in the background (logging) and simple script that can modify the appearance of .de sites

---
+++ plugin/my-background.js
@@ -0,0 +1,8 @@
+function logURL(requestDetails) {
+    console.log("Loading: " + requestDetails.url);
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+    logURL,
+    {urls: ["<all_urls>"]}
+);
