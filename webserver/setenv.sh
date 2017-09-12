ab14605d93e2c734abd00ab3970be17993208618
Steffen Remus <steremus@gmail.com>
2017-09-12 14:46:00 +0200

    cleanup

--- webserver/setenv.sh
+++ webserver/setenv.sh
@@ -13,20 +13,3 @@ export GERMANER_PORT="8080"
 
 export NER="corenlp" # germaner
 export NO_KEYWORDS="" # true
-
-export d_mysql="mysql:5.5.51"
-export n_mysql="sf-mysql"
-
-export d_corenlp="kisad/corenlp-german" # my-storyfinder-corenlp
-export n_corenlp="sf-corenlp"
-
-export d_ner="kisad/storyfinder-germaner-git" # kisad/storyfinder-germaner   my-storyfinder-germaner
-export n_ner="sf-germaner"
-
-export d_server="kisad/storyfinder-webserver" # my-storyfinder-server "storyfinder-server-local"
-export n_server="sf-server-kisad"
-
-export d_server_dev="node:6"
-export n_server_dev="sf-server-dev"
-
-alias firedev='/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox --profilemanager'
