a57f4d6973c026da101bdbe875aa6abfabbe917b
Steffen Remus <steremus@gmail.com>
2017-09-12 16:06:45 +0200

    minor

--- webserver/init_dev.sh
+++ webserver/init_dev.sh
@@ -2,7 +2,6 @@
 
 apt-get update && apt-get install -y libcairo2-dev libjpeg62-turbo-dev libpango1.0-dev libgif-dev build-essential
 
-
 cd /usr/src/app && npm install
 
 cd /usr/src/app/public/js && npm install
