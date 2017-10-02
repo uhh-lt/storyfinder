2a58bf82ad0a4df259df138a1069c6a5b494ed86
Steffen Remus <steremus@gmail.com>
2017-10-02 10:07:33 +0200

    revert entity highlighting fix

--- plugin/src/contentscript.js
+++ plugin/src/contentscript.js
@@ -196,36 +196,37 @@ function Storyfinder() {
             return b.caption.length - a.caption.length;
         });
 
-        //entities.forEach(entity => {
-        articleNodes.forEach(articleNode => {
-            let textNodes = getTextNodesIn(articleNode.el);
+        // FIXME: this loop over entities looks reduntant, but apparently it is important, find out why!
+        entities.forEach(entity => {
+            articleNodes.forEach(articleNode => {
+                let textNodes = getTextNodesIn(articleNode.el);
 
-            if (textNodes.length === 0) {
-                return;
-            }
+                if (textNodes.length === 0) {
+                    return;
+                }
 
-            entities.forEach(entity => {
-                let val = entity.caption;
+                entities.forEach(entity => {
+                    let val = entity.caption;
 
-                textNodes.forEach(textNode => {
-                    let txt = textNode.textContent;
+                    textNodes.forEach(textNode => {
+                        let txt = textNode.textContent;
 
-                    if (!_.isUndefined(txt.split)) {
-                        let split = new RegExp('([^A-Za-z0-9\-])(' + escapeStringRegexp(val) + ')([^\-A-Za-z0-9])', 'g');
-                        let replaced = txt.replace(split, '$1<sf-entity class="entity type-' + entity.type + '" data-entity-id="' + entity.id + '">$2</sf-entity>$3');
-                        if (txt !== replaced) {
-                            let newTextNode = document.createElement('sf-text-node');
-                            newTextNode.innerHTML = replaced;
+                        if (!_.isUndefined(txt.split)) {
+                            let split = new RegExp('([^A-Za-z0-9\-])(' + escapeStringRegexp(val) + ')([^\-A-Za-z0-9])', 'g');
+                            let replaced = txt.replace(split, '$1<sf-entity class="entity type-' + entity.type + '" data-entity-id="' + entity.id + '">$2</sf-entity>$3');
+                            if (txt !== replaced) {
+                                let newTextNode = document.createElement('sf-text-node');
+                                newTextNode.innerHTML = replaced;
 
-                            if (!_.isNull(textNode.parentElement)) {
-                                textNode.parentElement.replaceChild(newTextNode, textNode);
+                                if (!_.isNull(textNode.parentElement)) {
+                                    textNode.parentElement.replaceChild(newTextNode, textNode);
+                                }
                             }
                         }
-                    }
+                    });
                 });
             });
-        });
-        //});
+        }); // FIXME: end of the dubious loop
     }
 
     function activateHighlighting() {
